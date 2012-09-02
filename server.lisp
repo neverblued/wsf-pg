8233d77a3c95f9c8c6a27a2024bc21dda40375cf
Dmytro Pinskyy <demetrius@neverblued.info>
2012-09-02 21:21:25 +0300

    server password

--- server.lisp
+++ server.lisp
@@ -4,17 +4,33 @@
 
 (in-package #:wsf-pg)
 
-(defgeneric server-database (database-server))
+(defgeneric server-database
+    (database-server))
 
 (defclass database-server ()
   ((database :initarg :database :accessor server-database :initform nil)))
 
-(defmethod respond :around ((server database-server) request)
+(defmethod respond :around
+    ((server database-server) request)
   (aif (server-database server)
        (with-database it (call-next-method))
        (call-next-method)))
 
-(defmacro with-server-database (&body body)
+(defmacro with-server-database
+    (&body body)
   `(aif (server-database *server*)
         (with-database it ,@body)
         (error "Server ~a database is null" *server*)))
+
+(defclass database-server-system
+    (lisp-server) ())
+
+(defun password-pathname (system)
+  (join (asdf:system-source-directory system)
+        "/../../.postgres.pwd"))
+
+(defmethod postgrace::password ()
+  (let ((pathname (password-pathname (server-system *server*))))
+    (unless (probe-file pathname)
+      (error "Файл с паролем к БД '~a' не найден." pathname))
+    (values (pathname-content pathname))))
